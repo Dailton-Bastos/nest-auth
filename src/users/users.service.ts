46099f10c189f58c78cf7b749b194064bfdbb77f
Dailton Bastos <dailtonbastos@gmail.com>
2025-12-21 15:16:10 -0300

    feat(users): creating new users

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+/** biome-ignore-all lint/style/useImportType: <Nest can't resolve dependencies> */
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { CreateUserDto } from './dtos/create-user.dto'
+import { User } from './entities/user.entity'
+
+@Injectable()
+export class UsersService {
+	constructor(
+		@InjectRepository(User)
+		private readonly userRepository: Repository<User>
+	) {}
+
+	async create(createUserDto: CreateUserDto) {
+		const user = this.userRepository.create(createUserDto)
+
+		await this.userRepository.save(user)
+
+		return user
+	}
+}
