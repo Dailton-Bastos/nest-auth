e1da7edef4e08471a846d3675f9a287021fccf4a
Dailton Bastos <dailtonbastos@gmail.com>
2025-12-21 17:42:56 -0300

    test(e2e): add app setup

---
+++ test/setup.ts
@@ -0,0 +1,43 @@
+import type { INestApplication } from '@nestjs/common'
+import { ConfigModule, type ConfigType } from '@nestjs/config'
+import { Test, type TestingModule } from '@nestjs/testing'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import type { App } from 'supertest/types'
+import { AuthModule } from '../src/auth/auth.module'
+import databaseConfig from '../src/common/config/database.config'
+
+let app: INestApplication<App>
+
+global.beforeEach(async () => {
+	const module: TestingModule = await Test.createTestingModule({
+		imports: [
+			ConfigModule.forRoot({
+				isGlobal: true
+			}),
+			TypeOrmModule.forRootAsync({
+				imports: [ConfigModule.forFeature(databaseConfig)],
+				inject: [databaseConfig.KEY],
+				useFactory: (config: ConfigType<typeof databaseConfig>) => ({
+					type: 'postgres',
+					host: config.host,
+					port: config.port,
+					username: config.username,
+					password: config.password,
+					database: 'nest_auth_testing',
+					synchronize: true,
+					autoLoadEntities: true,
+					dropSchema: true
+				})
+			}),
+			AuthModule
+		]
+	}).compile()
+
+	app = module.createNestApplication()
+
+	await app.init()
+})
+
+global.afterEach(async () => {
+	await app.close()
+})
