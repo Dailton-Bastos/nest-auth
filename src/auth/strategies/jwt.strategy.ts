9eb5d687899759af488f3df1020ac07916660d19
Dailton Bastos <dailtonbastos@gmail.com>
2026-01-04 17:11:15 -0300

    fix(auth): inject jwt strategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,12 @@
 /** biome-ignore-all lint/style/useImportType: <Nest can't resolve dependencies> */
+import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { jwtConstants } from 'src/common/constants'
 import type { TokenPayload } from 'src/common/interfaces/token-payload.interface'
 import { UsersService } from 'src/users/users.service'
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 	constructor(private readonly usersService: UsersService) {
 		super({
